1f473165116a7cb46da2230e27f52976df005133
diksha <65006493+diksha0799@users.noreply.github.com>
2020-11-20 23:29:45 +0530

    Update settings.py

--- weather app/the_weather/settings.py
+++ weather app/the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cfb-21v4-bi6s!(*ao=v(&sfni&+d-y6exuwgc+!7_f0j(&a(&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-pridiction-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
